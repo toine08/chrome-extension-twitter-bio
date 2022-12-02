e4a7b4edc3ff56e82d471c05408886feeb3f267d
toto <riedotoine@gmail.com>
2022-12-02 17:49:18 +0100

    add content and improve manifest

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -8,7 +8,22 @@ const getKey = () => {
       });
     });
   };
-
+const sendMessage=(content)=>{
+    chrome.tabs.query({active: true, currentWindow:true}, (tabs)=>{
+        const activeTab = tabs[0].id;
+        
+        chrome.tabs.sendMessage(
+            activeTab,
+            {message:'inject', content},
+            (response) => {
+                if (!response) console.log('There is no active window. Please make sure the intended window is in focus.');
+                if (response.status === "failed"){
+                    console.log('injection failed.');
+                }
+            }
+        );
+    });
+};
 const generate = async (prompt) => {
   const key = await getKey();
   const url = "https://api.openai.com/v1/completions";
@@ -36,9 +51,11 @@ const generate = async (prompt) => {
 
 const generateCompletionAction = async (info) => {
   try {
+
+    sendMessage('generating...');
     const { selectionText } = info;
     const basePromptPrefix = `
-      Improve this bio with humour and give me 5 exemples of bio:\n
+      Improve this bio with humour:\n
       `;
 
     // Add this to call GPT-3
@@ -47,9 +64,12 @@ const generateCompletionAction = async (info) => {
     );
 
     // Let's see what we get!
-    console.log(baseCompletion.text);
+    sendMessage(baseCompletion.text);
+
+
   } catch (error) {
     console.log(error);
+    sendMessage(error.toString());
   }
 };
 
